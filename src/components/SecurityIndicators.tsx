6e0a7054a4bd702b0e401d1ba3470a32393b1284
Andres <jefferson.espejo@udea.edu.co>
2024-11-11 17:31:06 -0500

    Update security indicators

--- src/components/SecurityIndicators.tsx
+++ src/components/SecurityIndicators.tsx
@@ -4,22 +4,24 @@ const SecurityIndicators = () => {
   return (
     <div className='security-container'>
       <div className='security-text'>
-        <h2>
-          Indicadores sociales y de seguridad: Evolución de la Criminalidad y la
-          Violencia
-        </h2>
+        <h2>Indicadores sociales</h2>
         <p>
-          En esta sección se analizarán los indicadores de seguridad de
-          Medellín, incluyendo estadísticas sobre hurtos, violencia sexual,
-          violencia doméstica, homicidios y accidentes. Estos datos permitirán
-          evaluar cómo han evolucionado estos problemas a lo largo de los años,
-          proporcionando una visión integral de las condiciones de seguridad en
-          la ciudad y su impacto en la calidad de vida de los habitantes y en la
-          percepción de Medellín como un destino seguro para los turistas.
+          En esta sección se analizarán los indicadores sociales clave de
+          Medellín, incluyendo métricas fundamentales como la esperanza de vida,
+          el índice de desarrollo humano, la pobreza multidimensional y la tasa
+          de desempleo. Estos datos nos permitirán comprender la evolución del
+          bienestar social en la ciudad, evaluando aspectos cruciales como el
+          acceso a servicios básicos, oportunidades laborales y calidad de vida
+          de los habitantes. A través de este análisis, podremos entender mejor
+          los avances y desafíos en el desarrollo social de Medellín y su
+          impacto en la construcción de una ciudad más equitativa e inclusiva.
         </p>
       </div>
       <div className='security-image'>
-        <img src={'./criminalidad-1.jpg'} alt='Indicadores de Seguridad' />
+        <img
+          src={'./tipos-de-indicadores.jpg'}
+          alt='Indicadores de Seguridad'
+        />
       </div>
     </div>
   );
